f309d50a9dd404269128a7c1e27d9f8fe481f0e4
Serge <sergey@batalov.email>
2022-11-09 23:28:04 +0000

    Созданы приложения Модели в процессе создания

--- silant/production/models.py
+++ silant/production/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+class Mashina(models.Model):
+    zavodsk_nomer = models.CharField(max_length = 255)
+    model_tech = models.ForeignKey(Model_tech, on_delete=models.CASCADE)
+    model_dvig = models.ForeignKey(Model_dvig, on_delete=models.CASCADE)
+    nomer_dvig = models.CharField(max_length = 255)
+    model_trasm = models.ForeignKey(Model_trasm, on_delete=models.CASCADE)
+    nomer_transm = models.CharField(max_length = 255)
+    model_vmost = models.ForeignKey(Model_vmost, on_delete=models.CASCADE)
+    nomer_vmost = models.CharField(max_length = 255)
+    model_umost = models.ForeignKey(Model_tech, on_delete=models.CASCADE)
+    nomer_umost = models.CharField(max_length = 255)
+    dogovor_postavki = models.CharField(max_length = 255)
+    data_otgruzki = models.DateField(auto_now_add = True)
+    polzovatel = models.CharField(max_length = 255)
+    adres_polz = models.CharField(max_length = 255)
+    komplekt = models.CharField(max_length = 255)
+    klient = models.CharField(max_length = 255)
+    servis_partn = models.CharField(max_length = 255)
+
+class Model_tech(models.Model):
+    nazvan = models.CharField(max_length = 255)
+    opisanie = models.CharField(max_length = 255)
+
+class Model_dvig(models.Model):
+    nazvan = models.CharField(max_length = 255)
+    opisanie = models.CharField(max_length = 255)
+
+class Model_trasm(models.Model):
+    nazvan = models.CharField(max_length = 255)
+    opisanie = models.CharField(max_length = 255)
+
+class Model_vmost(models.Model):
+    nazvan = models.CharField(max_length = 255)
+    opisanie = models.CharField(max_length = 255)
+
+class Model_umost(models.Model):
+    nazvan = models.CharField(max_length = 255)
+    opisanie = models.CharField(max_length = 255)
--- silant/service/models.py
+++ silant/service/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from silant.production.models import Mashina
+
+
+class Servis (models.Model):
+    nazvanie = models.CharField(max_length = 255)
+    opisanie = models.CharField(max_length = 255)
+
+class To(models.Model):
+    id = models.ForeignKey(Mashina, to_field = zavodsk_nomer, on_delete=models.CASCADE)
+    vid_to = models.CharField(max_length = 255)
+    data_to = models.DateField(auto_now_add = True)
+    narabotka = models.IntegerField()
