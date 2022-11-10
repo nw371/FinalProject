9a74321ee908e8f5ffd6882697fa81fe470f7152
Serge <sergey@batalov.email>
2022-11-10 22:26:08 +0000

    Закончил написание моделей

--- silant/production/models.py
+++ silant/production/models.py
@@ -1,23 +1,6 @@
 from django.db import models
 
-class Mashina(models.Model):
-    zavodsk_nomer = models.CharField(max_length = 255)
-    model_tech = models.ForeignKey(Model_tech, on_delete=models.CASCADE)
-    model_dvig = models.ForeignKey(Model_dvig, on_delete=models.CASCADE)
-    nomer_dvig = models.CharField(max_length = 255)
-    model_trasm = models.ForeignKey(Model_trasm, on_delete=models.CASCADE)
-    nomer_transm = models.CharField(max_length = 255)
-    model_vmost = models.ForeignKey(Model_vmost, on_delete=models.CASCADE)
-    nomer_vmost = models.CharField(max_length = 255)
-    model_umost = models.ForeignKey(Model_tech, on_delete=models.CASCADE)
-    nomer_umost = models.CharField(max_length = 255)
-    dogovor_postavki = models.CharField(max_length = 255)
-    data_otgruzki = models.DateField(auto_now_add = True)
-    polzovatel = models.CharField(max_length = 255)
-    adres_polz = models.CharField(max_length = 255)
-    komplekt = models.CharField(max_length = 255)
-    klient = models.CharField(max_length = 255)
-    servis_partn = models.CharField(max_length = 255)
+from silant.service.models import Servis
 
 class Model_tech(models.Model):
     nazvan = models.CharField(max_length = 255)
@@ -38,3 +21,23 @@ class Model_vmost(models.Model):
 class Model_umost(models.Model):
     nazvan = models.CharField(max_length = 255)
     opisanie = models.CharField(max_length = 255)
+
+class Mashina(models.Model):
+    zavodsk_nomer = models.CharField(max_length = 255)
+    model_tech = models.ForeignKey(Model_tech, on_delete=models.CASCADE)
+    model_dvig = models.ForeignKey(Model_dvig, on_delete=models.CASCADE)
+    nomer_dvig = models.CharField(max_length = 255)
+    model_trasm = models.ForeignKey(Model_trasm, on_delete=models.CASCADE)
+    nomer_transm = models.CharField(max_length = 255)
+    model_vmost = models.ForeignKey(Model_vmost, on_delete=models.CASCADE)
+    nomer_vmost = models.CharField(max_length = 255)
+    model_umost = models.ForeignKey(Model_tech, on_delete=models.CASCADE)
+    nomer_umost = models.CharField(max_length = 255)
+    dogovor_postavki = models.CharField(max_length = 255)
+    data_otgruzki = models.DateField(auto_now_add = True)
+    polzovatel = models.CharField(max_length = 255)
+    adres_polz = models.CharField(max_length = 255)
+    komplekt = models.CharField(max_length = 255)
+    klient = models.CharField(max_length = 255)
+    servis_partn = models.ForeignKey(Servis, on_delete=models.CASCADE)
+
--- silant/service/models.py
+++ silant/service/models.py
@@ -3,12 +3,32 @@ from django.db import models
 from silant.production.models import Mashina
 
 
-class Servis (models.Model):
-    nazvanie = models.CharField(max_length = 255)
-    opisanie = models.CharField(max_length = 255)
+class Vid_to(models.Model):
+    nazvanie = models.CharField(max_length=255)
+    opisanie = models.CharField(max_length=255)
+
+class Otkaz(models.Model):
+    nazvanie = models.CharField(max_length=255)
+    opisanie = models.CharField(max_length=255)
+
+class Vosstanovlenie(models.Model):
+    nazvanie = models.CharField(max_length=255)
+    opisanie = models.CharField(max_length=255)
+
+class Servis(models.Model):
+    nazvanie = models.CharField(max_length=255)
+    opisanie = models.CharField(max_length=255)
 
 class To(models.Model):
     id = models.ForeignKey(Mashina, to_field = zavodsk_nomer, on_delete=models.CASCADE)
-    vid_to = models.CharField(max_length = 255)
+    vid_to = models.ForeignKey(Vid_to, on_delete=models.CASCADE)
     data_to = models.DateField(auto_now_add = True)
     narabotka = models.IntegerField()
+    uzel_otkaza = models.ForeignKey(Otkaz, on_delete=models.CASCADE)
+    # opisanie_otkaza =
+    vosstanovlenie = models.ForeignKey(Vosstanovlenie, on_delete=models.CASCADE)
+    chasti = models.CharField(max_length = 255)
+    data_vosstanov = models.CharField(max_length = 255)
+    vrem_prost = models.CharField(max_length = 255)
+    servis = models.ForeignKey(Servis, on_delete=models.CASCADE)
+
